1e15052ff7f25194ecd23b9a4753a0ad73ccaa57
Xavier Tonnellier <72013068+Xaviertonnellier6@users.noreply.github.com>
2022-01-16 00:54:06 +0100

    Update rolland_gamos_cs.py

--- rolland_gamos_cs.py
+++ rolland_gamos_cs.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 def find_id_from_name(player_name, time_sleep = 0.3):
     page = get_parsed_page(f"https://www.hltv.org/search?query={player_name}")
     time.sleep(time_sleep)
-    return page.find("table", {"class": "table"}).find('a')['href'].split("/")[2]
+    return page.find("table", {"class": "table"}).find('a')['href'].split("/")[2].lower()
 
 
 def get_parsed_page(url):
@@ -17,7 +17,7 @@ def get_parsed_page(url):
     }
     return BeautifulSoup(requests.get(url, headers=headers).text, "lxml")
 
-def _find_lineup(team_name, team_id, player_1, player_2):
+def _find_lineup(team_name, team_id, player_1, player_2, display):
     page = get_parsed_page(f"https://www.hltv.org/stats/teams/lineups/{team_id}/a")
     list_teammate_lineup = []
     lineup_containers = page.find_all("div", {"class": "lineup-container"})
@@ -27,16 +27,17 @@ def _find_lineup(team_name, team_id, player_1, player_2):
         for teammate in lineup:
             list_teammate_lineup.append(teammate.find("div", {"class": "text-ellipsis"}).text.lower())
         if player_1 in list_teammate_lineup and player_2 in list_teammate_lineup:
-            print("They played together in " + team_name + " (" + lineup_container.find_all("span", {"data-time-format": "MMM yyyy"})[0].text
-                    + "-" + lineup_container.find_all("span", {"data-time-format": "MMM yyyy"})[1].text + ") " + str(list_teammate_lineup))
+            if display:
+                print("They played together in " + team_name + " (" + lineup_container.find_all("span", {"data-time-format": "MMM yyyy"})[0].text
+                        + "-" + lineup_container.find_all("span", {"data-time-format": "MMM yyyy"})[1].text + ") " + str(list_teammate_lineup))
             played_together = True
         list_teammate_lineup = []
-    if not played_together:
+    if not played_together and display:
         print("They not played together in " + team_name)
     return played_together
 
 
-def players_played_together(name_first_player, name_second_player, time_sleep = 0.3):
+def players_played_together(name_first_player, name_second_player, display = True, time_sleep = 0.3):
     player_id = find_id_from_name(name_first_player, time_sleep)
     played_together = False
     page = get_parsed_page(f"https://www.hltv.org/player/{player_id}/a#tab-teamsBox")
@@ -50,7 +51,7 @@ def players_played_together(name_first_player, name_second_player, time_sleep = 
         list_team_id.append(team.find("td", {"class": "team-name-cell"}).find('a')['href'].split("/")[2])
         list_team_name.append(team.find("td", {"class": "team-name-cell"}).find('a')['href'].split("/")[3])
     for team_id, team_name in zip(list_team_id, list_team_name):
-        if _find_lineup(team_name, team_id, name_first_player, name_second_player):
+        if _find_lineup(team_name, team_id, name_first_player.lower(), name_second_player.lower(), display):
             played_together = True
         time.sleep(time_sleep)
     return played_together
